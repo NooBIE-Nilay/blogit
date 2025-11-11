36cd3f0795f41ca389e9359cdb0493a9c0105007
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-11-11 11:08:57 +0530

    Added Neetoui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,13 @@
 import "../stylesheets/application.scss";
 import ReactRailsUJS from "react_ujs";
 import App from "../src/App";
-
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
-
 initializeLogger();
 setAuthHeaders();
 
 const componentsContext = { App };
-ReactRailsUJS.getConstructor = (name) => {
+
+ReactRailsUJS.getConstructor = name => {
   return componentsContext[name];
 };
