2e4edbaf42175cced959c4e76976e26590b710c5
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-11-28 10:40:28 +0530

    Initialized react-query client instance

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -7,8 +7,10 @@ import { PrivateRoute } from "components/commons";
 import Dashboard from "components/Dashboard";
 import { CreatePost, ShowPost, EditPost } from "components/Posts";
 import { either, isEmpty, isNil } from "ramda";
+import { QueryClientProvider } from "react-query";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import queryClient from "utils/queryClient";
 import { getFromLocalStorage } from "utils/storage";
 
 const App = () => {
@@ -16,22 +18,24 @@ const App = () => {
   const isLoggedIn = !either(isNil, isEmpty)(authToken);
 
   return (
-    <Router>
-      <ToastContainer />
-      <Switch>
-        <Route exact component={EditPost} path={routes.posts.edit} />
-        <Route exact component={ShowPost} path={routes.posts.show} />
-        <Route exact component={CreatePost} path={routes.posts.create} />
-        <Route exact component={Signup} path={routes.signup} />
-        <Route exact component={Login} path={routes.login} />
-        <PrivateRoute
-          component={Dashboard}
-          condition={isLoggedIn}
-          path={routes.root}
-          redirectRoute={routes.login}
-        />
-      </Switch>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <ToastContainer />
+        <Switch>
+          <Route exact component={EditPost} path={routes.posts.edit} />
+          <Route exact component={ShowPost} path={routes.posts.show} />
+          <Route exact component={CreatePost} path={routes.posts.create} />
+          <Route exact component={Signup} path={routes.signup} />
+          <Route exact component={Login} path={routes.login} />
+          <PrivateRoute
+            component={Dashboard}
+            condition={isLoggedIn}
+            path={routes.root}
+            redirectRoute={routes.login}
+          />
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
