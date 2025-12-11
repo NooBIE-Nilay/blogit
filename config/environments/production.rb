b9f95cd4692ca915ea7ea1fec50a0f3ba90bd8b0
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-12-11 11:38:35 +0530

    Added redis to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,12 @@ Rails.application.configure do
   config.active_storage.service = :google
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.mount_path = "/cable"
+  config.action_cable.url = ENV.fetch("CABLE_URL", "wss://blogit-noobie.onrender.com/cable")
+  config.action_cable.allowed_request_origins = [
+    "https://blogit-noobie.onrender.com",
+    /https:\/\/blogit-noobie\.onrender\.com/
+  ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
